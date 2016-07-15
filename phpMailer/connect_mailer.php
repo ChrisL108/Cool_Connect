584293d1f9c48ab3ddb817ecd952452b44a086f9
Christopher Lacaille <Christopher Lacaille>
2016-07-15 03:39:50 -0500

    add PHPMailer

---
+++ phpMailer/connect_mailer.php
@@ -0,0 +1,49 @@
+<!-- connect_mailer -->
+
+<?php
+	require 'PHPMailerAutoload.php';
+
+
+    // Get the form fields and remove whitespace.
+    $name = strip_tags(trim($_POST["name"]));
+            $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    // $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
+
+    $email = trim($_POST["email"]);
+
+	$mail = new PHPMailer;
+
+	//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+	$mail->isSMTP();                                      // Set mailer to use SMTP
+	$mail->Host = 'mail.chrislacaille.com';       // Specify main and backup SMTP servers
+	$mail->SMTPAuth = true;                               // Enable SMTP authentication
+	$mail->Username = 'chris@chrislacaille.com';                 // SMTP username
+	$mail->Password = 'saitek150';                           // SMTP password
+	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+	$mail->Port = 587;                                    // TCP port to connect to
+
+	$mail->setFrom('chrislacaille5@gmail.com', 'CoolConnect Mailer');
+	// $mail->addAddress($email, 'Joe User');     // Add a recipient
+	$mail->addAddress($email);               // Name is optional
+	// $mail->addReplyTo('info@example.com', 'Information');
+	// $mail->addCC('cc@example.com');
+	// $mail->addBCC('bcc@example.com');
+
+	// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+	// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+	$mail->isHTML(true);                                  // Set email format to HTML
+
+	$mail->Subject = 'Here is the subject';
+	$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+	$mail->AltBody = $message;
+
+	if(!$mail->send()) {
+	    echo 'Message could not be sent.';
+	    echo 'Mailer Error: ' . $mail->ErrorInfo;
+	} else {
+	    echo 'Message has been sent';
+	}
+
+
