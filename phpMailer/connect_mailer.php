6a975001fe89dcdd2b90d0af63dbf526460e3bf7
Christopher Lacaille <Christopher Lacaille>
2016-07-15 23:45:27 -0500

    comment out server information for testing

--- phpMailer/connect_mailer.php
+++ phpMailer/connect_mailer.php
@@ -16,17 +16,17 @@
 
 	$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-	$mail->isSMTP();                                      // Set mailer to use SMTP
-	$mail->Host = 'ns8341.hostgator.com';       // Specify main and backup SMTP servers
-	$mail->SMTPAuth = true;                               // Enable SMTP authentication
-	$mail->Username = 'chris@chrislacaille.com';                 // SMTP username
-	$mail->Password = 'saitek150';                           // SMTP password
-	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-	$mail->Port = 587;                                    // TCP port to connect to
+	// $mail->isSMTP();                                      // Set mailer to use SMTP
+	// $mail->Host = 'ns8341.hostgator.com';       // Specify main and backup SMTP servers
+	// $mail->SMTPAuth = true;                               // Enable SMTP authentication
+	// $mail->Username = 'chris@chrislacaille.com';                 // SMTP username
+	// $mail->Password = 'saitek150';                           // SMTP password
+	// $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+	// $mail->Port = 587;                                    // TCP port to connect to
 
 	$mail->setFrom('chrislacaille5@gmail.com', $name);
-	// $mail->addAddress($email, 'Joe User');     // Add a recipient
-	$mail->addAddress($email);               // Name is optional
+	$mail->addAddress('example@example.com', 'Joe User');     // Add a recipient
+	// $mail->addAddress($email);               // Name is optional
 	// $mail->addReplyTo('info@example.com', 'Information');
 	// $mail->addCC('cc@example.com');
 	// $mail->addBCC('bcc@example.com');
