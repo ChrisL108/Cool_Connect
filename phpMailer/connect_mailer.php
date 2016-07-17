4b4bfaa8809dd5e80404ec1a4cdb38d28791e5a7
ChrisL108 <chrislacaille5@gmail.com>
2016-07-16 19:06:49 -0500

    update sender to display in header

--- phpMailer/connect_mailer.php
+++ phpMailer/connect_mailer.php
@@ -10,36 +10,29 @@
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     $message = trim($_POST["message"]);
 
-    // $email = trim($_POST["email"]);
+    $sender = trim($_POST["sender"]);
 
 	$mail = new PHPMailer;
 
 	$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-<<<<<<< HEAD
+
 	$mail->isSMTP();                                      // Set mailer to use SMTP
-	$mail->Host = 'ns8341.hostgator.com';       // Specify main and backup SMTP servers
+	$mail->Host = 'ns8341.hostgator.com; ns8342.hostgator.com';       // Specify main and backup SMTP servers
 	$mail->SMTPAuth = true;                               // Enable SMTP authentication
 	$mail->Username = 'chris@chrislacaille.com';                 // SMTP username
 	$mail->Password = 'saitek150';                           // SMTP password
 	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 	$mail->Port = 587;                                    // TCP port to connect to
 
-	$mail->setFrom('chrislacaille5@gmail.com', $name);
+	$mail->setFrom("chrislacaille5@gmail.com", $name);
 	// $mail->addAddress('example@example.com', 'Joe User');     // Add a recipient
 	$mail->addAddress($email);               // Name is optional
 	// $mail->addReplyTo('info@example.com', 'Information');
 	// $mail->addCC('cc@example.com');
 	// $mail->addBCC('bcc@example.com');
-=======
 
-
-	$mail->setFrom('chrislacaille5@gmail.com', $name);
-	$mail->addAddress($email);               // Name is optional
-	// $mail->addAddress('example@example.com', 'Joe User');     // Add a recipient
->>>>>>> 20b45b5c904fab087b290948d51d6a22ad6bbb7a
-
-	$mailsubject = 'Message from ' . $name;
+	$mailsubject = 'Sender Email: ' . $sender;
 
 	$mail->Subject = $mailsubject ;
 	$mail->Body    = $message;
